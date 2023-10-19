4f7828b407ac23573347bbc7f1146dfb49ac73e8
RubenIrimiaPerez <rubenbaloncesto10@gmail.com>
2023-10-19 14:10:48 +0200

    poner foto de fondo y logo

--- app/src/main/java/com/RubenIrimiaPerez/firstdesign/SplashScreen.java
+++ app/src/main/java/com/RubenIrimiaPerez/firstdesign/SplashScreen.java
@@ -20,9 +20,10 @@ public class SplashScreen extends AppCompatActivity {
         Glide.with(this)
                 .load("https://images.unsplash.com/photo-1533240332313-0db49b459ad6?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1974&q=80")
                 //.load(R.drawable.joel)
-                .transition(DrawableTransitionOptions.withCrossFade(100))
+                .transition(DrawableTransitionOptions.withCrossFade(2000))
                 .centerCrop()
                 .into(background);
+
 
     }
 }
